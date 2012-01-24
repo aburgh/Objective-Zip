4749c0652d6f1ca9342e7187f19eddea06bdb5bd
Aaron Burghardt <aburgh@mac.com>
2012-01-24 12:23:56 -0500

    Added static library and framework for Intel.

---
+++ Objective-Zip Framework/Objective_Zip.h
@@ -0,0 +1,12 @@
+//
+//  Objective_Zip.h
+//  Objective-Zip
+//
+//  Created on 1/24/12.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Objective_Zip : NSObject
+
+@end
---
+++ Objective-Zip Framework/Objective_Zip.m
@@ -0,0 +1,22 @@
+//
+//  Objective_Zip.m
+//  Objective-Zip
+//
+//  Created on 1/24/12.
+//
+
+#import "Objective_Zip.h"
+
+@implementation Objective_Zip
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
